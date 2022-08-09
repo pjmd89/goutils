64030f60cbc09f09f719da93d2b4d97ffe782129
Pablo Muñoz <pjmd89@gmail.com>
2022-08-09 12:59:00 -0400

    update

--- dbutils/db.go
+++ dbutils/db.go
@@ -6,7 +6,7 @@ type DBInterface interface{
 	Close() (err error)
 	Create(inputs interface{}, opts interface{}, results interface{})( err  error)
 	Read(where interface{}, opts interface{}, results interface{})( err  error)
-	Update(inputs interface{}, where interface {}, opts interface{}, results []*interface{}) ( err  error)
+	Update(inputs interface{}, where interface {}, opts interface{}, results interface{}) ( err  error)
 	Delete(where interface {}, opts interface{}, results interface{}) ( err  error)
 	Count(where interface {}, opts interface{}, results interface{}) ( err  error)
 	SetDatabase(db string)
