16fc53be06d0d1ac7bc5feb52879952cb4f31359
Pablo <pjmd89@gmail.com>
2022-09-16 01:50:12 -0400

    update

--- dbutils/db.go
+++ dbutils/db.go
@@ -1,21 +1,23 @@
 package dbutils
 
 import "fmt"
-type Model interface{
-	Create(inputs map[string]interface{}, opts interface{})									(r interface{}, err error)
-	Read(where interface{}, opts interface{})												( r interface{}, err  error )
-	Update(inputs map[string]interface{}, where interface {}, opts interface{})				( r interface{}, err  error )
-	Delete(where interface {}, opts interface{})											( r interface{}, err  error )
-	Count(where interface {}, opts interface{})												( r int64, err  error )
+
+type ModelInterface interface {
+	Create(inputs map[string]interface{}, opts interface{}) (r interface{}, err error)
+	Read(where interface{}, opts interface{}) (r interface{}, err error)
+	Update(inputs map[string]interface{}, where interface{}, opts interface{}) (r interface{}, err error)
+	Delete(where interface{}, opts interface{}) (r interface{}, err error)
+	Count(where interface{}, opts interface{}) (r int64, err error)
+	Init(self interface{}, db DBInterface)
 }
-type DBInterface interface{
+type DBInterface interface {
 	Connect() (err error)
 	Close() (err error)
-	Create(inputs interface{}, collection string, opts interface{})							( results interface{}, err  error )
-	Read(where interface{}, collection string, opts interface{} )							( results interface{}, err  error )
-	Update(inputs interface{}, where interface {}, collection string, opts interface{}) 	( results interface{}, err  error )
-	Delete(where interface {}, collection string, opts interface{}) 						( results interface{}, err  error )
-	Count(where interface {}, collection string, opts interface{})	 						( results interface{}, err  error )
+	Create(inputs interface{}, collection string, opts interface{}) (results interface{}, err error)
+	Read(where interface{}, collection string, opts interface{}) (results interface{}, err error)
+	Update(inputs interface{}, where interface{}, collection string, opts interface{}) (results interface{}, err error)
+	Delete(where interface{}, collection string, opts interface{}) (results interface{}, err error)
+	Count(where interface{}, collection string, opts interface{}) (results interface{}, err error)
 	SetDatabase(db string)
 	SetCollection(collection string)
 	GetClient() (client interface{})
@@ -23,45 +25,46 @@ type DBInterface interface{
 }
 type DB struct {
 	DBInterface
-	Host 			string		`json:"host"`
-	Port 			string		`json:"port"`
-	User 			string		`json:"user"`
-	Pass 			string		`json:"pass"`
-	DataBase   		string		`json:"db"`
-	Reconnect		bool		`json:"reconnect"`
-	database 		string
-	collection 		string
-	OnDatabase		func(currentDB string, currentCollection string) ( string )
+	Host       string `json:"host"`
+	Port       string `json:"port"`
+	User       string `json:"user"`
+	Pass       string `json:"pass"`
+	DataBase   string `json:"db"`
+	Reconnect  bool   `json:"reconnect"`
+	database   string
+	collection string
+	OnDatabase func(currentDB string, currentCollection string) string
 }
-func(o *DB)Create(inputs interface{}, collection string, opts interface{})( results interface{}, err  error){
+
+func (o *DB) Create(inputs interface{}, collection string, opts interface{}) (results interface{}, err error) {
 	err = fmt.Errorf("No declared method")
-	return results, err;
+	return results, err
 }
-func(o *DB)Read(where interface{}, collection string, opts interface{}, results interface{} )( err  error){
+func (o *DB) Read(where interface{}, collection string, opts interface{}, results interface{}) (err error) {
 	err = fmt.Errorf("No declared method")
-	return err;
+	return err
 }
-func(o *DB)Update(inputs interface{}, where interface {}, collection string, opts interface{}) (results interface{},  err  error){
+func (o *DB) Update(inputs interface{}, where interface{}, collection string, opts interface{}) (results interface{}, err error) {
 	err = fmt.Errorf("No declared method")
-	return results, err;
+	return results, err
 }
-func(o *DB)Delete(where interface {}, collection string, opts interface{}) (results interface{},  err  error){
+func (o *DB) Delete(where interface{}, collection string, opts interface{}) (results interface{}, err error) {
 	err = fmt.Errorf("No declared method")
-	return results, err;
+	return results, err
 }
-func(o *DB)Count(where interface {}, collection string, opts interface{}) (results interface{},  err  error){
+func (o *DB) Count(where interface{}, collection string, opts interface{}) (results interface{}, err error) {
 	err = fmt.Errorf("No declared method")
-	return results, err;
+	return results, err
 }
-func(o *DB)SetDatabase(db string){
+func (o *DB) SetDatabase(db string) {
 	o.database = db
 }
-func(o *DB)GetDatabase() string{
+func (o *DB) GetDatabase() string {
 	return o.database
 }
-func(o *DB)SetCollection(collection string){
+func (o *DB) SetCollection(collection string) {
 	o.collection = collection
 }
-func(o *DB)GetCollection() string{
+func (o *DB) GetCollection() string {
 	return o.collection
 }
