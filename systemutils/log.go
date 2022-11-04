0420e4d7410804eb5431e8f47fcee47cf73bbf4a
Pablo <pjmd89@gmail.com>
2022-11-03 21:56:42 -0400

    update

--- systemutils/log.go
+++ systemutils/log.go
@@ -4,32 +4,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/pjmd89/goutils/systemutils/debugmode"
 )
 
 type Log struct {
-	path     string
-	fileName string
-	logger   *log.Logger
+	path      string
+	fileName  string
+	extension string
+	logger    *log.Logger
 }
 
-func NewLog(path string, fileName string) (r *Log) {
-	err := os.Mkdir(path, 0666)
+func NewLog(filePath string) (r *Log) {
+	p, err := filepath.Abs(filePath)
+	if err != nil {
+		log.Fatal("log not loaded: " + err.Error())
+	}
+	path := filepath.Dir(p)
+	fileName := filepath.Base(p)
+	extension := filepath.Ext(fileName)
+	fileName = strings.TrimSuffix(fileName, filepath.Ext(fileName))
+	err = os.Mkdir(path, 0666)
 	if err != nil {
 		log.Fatal("log not loaded: " + err.Error())
 		return nil
 	}
 	r.path = path
 	r.fileName = fileName
+	r.extension = extension
 	return
 }
-func logFile(path string, fileName string) (r *os.File) {
+func logFile(path string, fileName string, extension string) (r *os.File) {
 	if !debugmode.Enabled {
 		year, month, day := time.Now().Date()
-		fileName := fmt.Sprintf(fileName+"-%v-%v-%v.log", day, month.String(), year)
-		file, err := os.OpenFile(path+"/"+fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		filePath := fmt.Sprintf(path+"/"+fileName+"-%v-%v-%v."+extension, day, month.String(), year)
+		file, err := os.OpenFile(filePath, os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Fatal("log not loaded: " + err.Error())
 		}
@@ -42,7 +54,7 @@ func logFile(path string, fileName string) (r *os.File) {
 }
 func (o *Log) Error(s string, vars ...any) (r *log.Logger) {
 	if !debugmode.Enabled {
-		file := logFile(o.path, o.fileName)
+		file := logFile(o.path, o.fileName, o.extension)
 		r = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Llongfile)
 	} else {
 		r = &log.Logger{}
@@ -53,7 +65,7 @@ func (o *Log) Error(s string, vars ...any) (r *log.Logger) {
 }
 func (o *Log) Warning(s string, vars ...any) (r *log.Logger) {
 	if !debugmode.Enabled {
-		file := logFile(o.path, o.fileName)
+		file := logFile(o.path, o.fileName, o.extension)
 		r = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Llongfile)
 	} else {
 		r = &log.Logger{}
@@ -64,7 +76,7 @@ func (o *Log) Warning(s string, vars ...any) (r *log.Logger) {
 }
 func (o *Log) Info(s string, vars ...any) (r *log.Logger) {
 	if !debugmode.Enabled {
-		file := logFile(o.path, o.fileName)
+		file := logFile(o.path, o.fileName, o.extension)
 		r = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Llongfile)
 	} else {
 		r = &log.Logger{}
@@ -75,7 +87,7 @@ func (o *Log) Info(s string, vars ...any) (r *log.Logger) {
 }
 func (o *Log) Fatal(s string, vars ...any) (r *log.Logger) {
 	if !debugmode.Enabled {
-		file := logFile(o.path, o.fileName)
+		file := logFile(o.path, o.fileName, o.extension)
 		r = log.New(file, "FATAL: ", log.Ldate|log.Ltime|log.Llongfile)
 	} else {
 		r = &log.Logger{}
